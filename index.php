d439531b2fddcd9a5ef9c1eaf87a46d431e4341a
AKM <aris.krisna17@gmail.com>
2022-07-06 09:08:53 +0700

    -y

--- index.php
+++ index.php
@@ -552,7 +552,7 @@
 										 <i class="icon-pen2"></i>
 									  </div>
 									  <div class="timeline-label">
-										  <h2><a href="#">System Administrator at <b>PANDI</b></a> <span>09 Mei 2020 - Now (<?= $tpandidiff ?>)</span></h2>
+										  <h2><a href="#">System Administrator at <b>PANDI</b></a> <span>09 Mei 2020 - Now (<?= $tpandidiff->y ?>)</span></h2>
 										 <p>
 											 <strong>Jobdesk:</strong><br>
 										   <ul>
